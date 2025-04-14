56e849d4325c20f7bd4f6c10d1452b99ac6cce4d
root <root@mon4-ceph1-np.storage.unimelb.edu.au>
2025-04-15 09:33:15 +1000

    ceph 17.2.7 files

---
+++ jmamma/restart_osds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function wait_check () {
+  while [ "$(ceph --name client.bootstrap-osd --keyring /var/lib/ceph/bootstrap-osd/ceph.keyring --cluster ceph status | grep -c -e ERROR -e reduced -e peering -e activating)" -gt 0 ]; do
+    sleep 1
+  done
+}
+
+wait_check
+
+for unit in $(systemctl list-units | grep -E "loaded * active" | grep -oE "ceph-osd@([0-9]+).service"); do
+  # First, restart daemon(s)
+  systemctl restart "${unit}"
+done
+
+wait_check 
+
+
+
